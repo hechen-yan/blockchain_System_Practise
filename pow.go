7f0660f3811b8ffdcc41b9ffe67d8f3b695587ae
unknown <hechenworkspace@gmail.com>
2025-11-16 01:06:38 +0200

    RSA Verify

--- pow.go
+++ pow.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"math/rand"
+	"math/big"
 	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
+
 	var name = "Yan"
-	var nonce = rand.Int63()
+	var nonce int64
+	n, err := rand.Int(rand.Reader, big.NewInt(1<<60))
+	if err != nil {
+		panic(err)
+	}
+	nonce = n.Int64()
+
 	start := time.Now()
-	count := 0
+	count := int64(0)
+
+	privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	PublicKey := &privatekey.PublicKey
 
 	joinstring := name + strconv.FormatInt(nonce, 10)
 	hashstring := sha256.Sum256([]byte(joinstring)) //返回是byte[32]
@@ -29,5 +45,18 @@ func main() {
 
 	}
 	elasped := time.Since(start)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privatekey, crypto.SHA256, hashstring[:])
+	fmt.Println("签名为：", signature)
+	if err != nil {
+		panic(err)
+	}
+	err1 := rsa.VerifyPKCS1v15(PublicKey, crypto.SHA256, hashstring[:], signature)
+	if err1 != nil {
+		fmt.Println("验证失败")
+	} else {
+		fmt.Println("验证成功")
+	}
+
 	fmt.Println("耗时为：", elasped, "哈希值为：", hexstr, "哈希的内容为：", joinstring, "执行运算次数为：", count)
+
 }
